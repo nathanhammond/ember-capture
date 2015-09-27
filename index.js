33d9fadb3fca3fcc1bdecd2bba265046159f1450
Nathan Hammond <github.com@nathanhammond.com>
2015-09-27 12:09:02 -0700

    Play nicely with ember-cli-content-security-policy.

--- index.js
+++ index.js
@@ -38,5 +38,50 @@ module.exports = {
         }
       }
     }
+  },
+
+  serverMiddleware: function(config) {
+    var app = config.app;
+    var options = config.options;
+    var project = options.project;
+
+    // TODO: Switch on and off based upon the environment.
+
+    app.use(function(req, res, next) {
+      var appConfig = project.config(options.environment);
+
+      // If we're not using CSP headers, bail.
+      if (!appConfig || !appConfig.contentSecurityPolicyHeader || !appConfig.contentSecurityPolicy) {
+        next();
+        return;
+      }
+
+      var header = appConfig.contentSecurityPolicyHeader;
+      var headerConfig = appConfig.contentSecurityPolicy;
+      var normalizedHost = (options.captureHost ? options.captureHost : 'localhost');
+      var captureServerURL = 'http://' + normalizedHost + ':' + options.capturePort + '/';
+
+      // Make sure it exists.
+      headerConfig['connect-src'] = headerConfig['connect-src'] || '';
+
+      // Add ourselves to the list.
+      headerConfig['connect-src'] = headerConfig['connect-src'] + ' ' + captureServerURL;
+
+      // Collapse all of the values back into the header.
+      var headerValue = Object.keys(headerConfig).reduce(function(memo, value) {
+        return memo + value + ' ' + headerConfig[value] + '; ';
+      }, '');
+
+      res.removeHeader("Content-Security-Policy");
+      res.removeHeader("X-Content-Security-Policy");
+
+      res.removeHeader('Content-Security-Policy-Report-Only');
+      res.removeHeader('X-Content-Security-Policy-Report-Only');
+
+      res.setHeader(header, headerValue);
+      res.setHeader('X-' + header, headerValue);
+
+      next();
+    });
   }
 };
